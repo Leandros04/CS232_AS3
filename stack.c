c5359bb966663848731e7bd262e4819eaa1be0da
Christos2211 <166440025+Christos2211@users.noreply.github.com>
2024-11-08 17:43:04 +0200

    Update stack.c

--- stack.c
+++ stack.c
@@ -49,6 +49,112 @@ bool isEmpty(STACK *stack) {
 }
 
 /**
+ *  @brief Pushes a node inside the stack.
+ * 
+ *  This function pushes a node inside the stack and updates the fields of that node.
+ * 
+ *  @param stack A pointer to the stack.
+ *  @param square 2D array representing the Latin Square.
+ *  @param row Row where an element was last inserted.
+ *  @param col Column where an element was last inserted.
+ * 
+ */
+void push(STACK *stack, int **square, int row, int col){
+
+    NODE *p = NULL;
+    
+    if(stack == NULL){
+
+        printf("Error: The stack is empty.\n");
+        exit(-1);
+
+    }
+
+    p = (NODE *)malloc(sizeof(NODE)); //  Allocates memory for the new node.
+
+    if(p == NULL){
+
+        printf("Error: Failed to allocate memory for a new node.\n");
+        exit(-1);
+
+    }
+
+    //  Allocates memory for the 2D array in the node.
+    p->square = (int **)malloc(row*sizeof(int *));
+
+    if(p->square == NULL){
+
+        printf("Error: Failed to allocate memory for the 2D array.\n");
+        free(p); //  Frees the new node.
+        exit(-1);
+
+    }
+
+    //  Allocates memory for each row and copies the data.
+    for(int i=0; i<row; i++){
+
+        p->square[i] = (int *)malloc(col*sizeof(int));
+
+        if(p->square[i] == NULL){
+
+            printf("Error: Failed to allocate memory for the 2D array row.\n");
+
+            //  Frees the previously allocated memory in case of failure.
+            for(int j=0; j<i; j++){
+
+                free(p->square[j]);
+            
+            }
+
+            free(p->square);
+            free(p);
+            exit(-1);
+
+        }
+
+        //  Copies the data from the original array.
+        for(int j=0; j<col; j++){
+
+            p->square[i][j] = square[i][j];
+
+        }
+        
+    }
+
+    p->row = row;
+    p->col = col;
+    p->next = stack->top;
+    stack->top = p;
+    stack->size++;
+  
+}
+
+/**
+ *  @brief Pops a node from the stack.
+ * 
+ *  This function pops a node from the stack and updates the stack's fields.
+ * 
+ *  @param stack A pointer to the stack.
+ * 
+ */
+NODE *pop(STACK *stack){
+
+    if(stack == NULL || stack->size == 0){
+
+        printf("Error: The stack is empty. Cannot pop.\n");
+        return NULL;
+
+    }
+
+    NODE *p = stack->top;
+    stack->top = stack->top->next;
+    stack->size--;
+
+    return p;
+
+}
+
+/**
  *  @brief Returns the node at the top of the stack
  * 
  *  This function returns the top node of the stack without popping it 
